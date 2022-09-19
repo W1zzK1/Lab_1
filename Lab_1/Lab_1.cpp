11e5da1ed94820b3fbaa8b28f92cc9c49a563f7d
W1zzA <W1zzA@W1ZZK1>
2022-09-19 20:17:30 +0300

    Добавьте файлы проекта.

---
+++ Lab_1/Lab_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cin >> a >> b; //Ввод двух катетов
+	int aa = ((a * a) + (b * b)); //Вычтсление суммы квадратов катетов
+	c = sqrt(aa); //Вичисление корня из гипотенузы
+	cout << c << " " << "Гипотенуза" << endl;
+	int p = a + b + c;
+	cout << p << " " << "Периметр";
+	return 0;
+}
